af9af64bed0e41ab501c51a85aad5d116b99cd0a
Getter508 <gettersarah@gmail.com>
2018-11-04 15:08:49 -0500

    Add user attributes and allow in controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,7 +59,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :feature
 end
