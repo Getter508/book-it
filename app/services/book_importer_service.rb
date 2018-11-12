eb3c97e85990877fe5e9b302ca9c7876ba0a523d
Getter508 <gettersarah@gmail.com>
2018-11-11 22:04:22 -0500

    Update importer and isbns table

--- app/services/book_importer_service.rb
+++ app/services/book_importer_service.rb
@@ -20,7 +20,7 @@ class BookImporterService
         book = create_book(book_data)
         author = create_author(book_data, book.id)
         genre = create_genre(book_data, book.id)
-        isbn_obj = create_isbn(book_data, book.id)
+        isbn_obj = create_isbn(book_data, book.id, isbn)
       rescue StandardError => e
         errors << [isbn]
       end
@@ -37,17 +37,17 @@ class BookImporterService
       description: data[:description],
       cover: data[:cover]
     )
-    @books << book
     book
   end
 
   def create_author(data, book_id)
     data[:authors].each do |name|
-      author = Author.find_or_create_by(name: name)
-      authors << author
+      author = Author.where(name: data[:alt_author_names]).first
+      if author.nil?
+        author = Author.find_or_create_by(name: name)
+      end
       create_book_author(author, book_id)
     end
-    authors
   end
 
   def create_book_author(author, book_id)
@@ -57,79 +57,21 @@ class BookImporterService
   def create_genre(data, book_id)
     data[:genres].each do |name|
       genre = Genre.find_or_create_by(name: name)
-      genres << genre
       create_book_genre(genre, book_id)
     end
-    genres
   end
 
   def create_book_genre(genre, book_id)
     BookGenre.find_or_create_by(genre_id: genre.id, book_id: book_id)
   end
 
-  def create_isbn(data, book_id)
-    data[:isbns].each do |id|
-      isbn = Isbn.find_or_create_by(international_standard_book_number: id, book_id: book_id)
-      isbns << isbn
+  def create_isbn(data, book_id, primary_isbn)
+    data[:isbns].each do |international_standard_book_number|
+      isbn = Isbn.find_or_create_by(international_standard_book_number: international_standard_book_number, book_id: book_id)
+      if isbn == primary_isbn
+        isbn.primary_number = true
+        isbn.save
+      end
     end
-    isbns
   end
 end
-
-
-
-# class BookImporterService
-#   attr_reader :isbns
-#
-#   def self.call(isbns:)
-#     new(isbns: isbns).call
-#   end
-#
-#   def initialize(isbns:)
-#     @isbns = isbns
-#   end
-#
-#   def call
-#     @books = []
-#     @authors = []
-#     @genres = []
-#     isbns.each do |isbn|
-#       book_data = OpenLibrary.new(isbn).call
-#       book = create_book(book_data)
-#       author = create_author(book_data, book.id)
-#       genre = create_genre(book_data, book.id)
-#     end
-#   end
-#
-#   def create_book(data)
-#     # binding.pry
-#     book = Book.find_or_create_by(
-#       title: data[:title],
-#       subtitle: data[:subtitle],
-#       year: data[:year],
-#       pages: data[:pages],
-#       description: data[:description],
-#       cover: data[:cover]
-#     )
-#     @books << book
-#     book
-#   end
-#
-#   def create_author(data, book_id)
-#     data[:authors].each do |name|
-#       author = Author.find_or_create_by(name: name)
-#       @authors << author
-#       # binding.pry
-#       book_author = BookAuthor.find_or_create_by(book_id: book_id, author_id: author.id)
-#     end
-#   end
-#
-#   def create_genre(data, book_id)
-#     data[:genres].each do |name|
-#       genre = Genre.find_or_create_by(name: name)
-#       @genres << genre
-#       # binding.pry
-#       book_genre = BookGenre.find_or_create_by(book_id: book_id, genre_id: genre.id)
-#     end
-#   end
-# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_031337) do
+ActiveRecord::Schema.define(version: 2018_11_12_012909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_031337) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "international_standard_book_number", null: false
+    t.boolean "primary_number", default: false
     t.index ["book_id"], name: "index_isbns_on_book_id"
   end
 
